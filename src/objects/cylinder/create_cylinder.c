e795c31f1d270138d7ba62e8bd83909b443476e4
Alessandro Paghera <apaghera@1-E-23.42heilbronn.de>
2023-09-13 15:08:08 +0200

    cylinder done

--- src/objects/cylinder/create_cylinder.c
+++ src/objects/cylinder/create_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: apaghera <apaghera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 17:13:22 by apaghera          #+#    #+#             */
-/*   Updated: 2023/09/12 18:19:04 by apaghera         ###   ########.fr       */
+/*   Updated: 2023/09/13 15:05:37 by apaghera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,57 @@
 #include "scene.h"
 
 
+void	rotation_obj(t_vector *vector, t_shape *obj, int idx, int column)
+{
+	double	x;
+	double	y;
+	double	z;
+	char	**str;
+	char	**values;
+
+	str = *(char ***)vector_at(vector, idx);
+	if (view_from_to_valid(str[column]))
+	{
+		values = ft_split(str[column], ',');
+		x = ft_atof(values[0]);
+		y = ft_atof(values[1]);
+		z = ft_atof(values[2]);
+		shape_rotate(obj, x, y, z);
+		free_double_arr(values);
+	}
+}
+
+void	diam_height_obj(t_vector *vector, t_shape *obj, int idx, int column)
+{
+	double	x;
+	double	y;
+	double	z;
+	char	**str;
+	char	**values;
+
+	str = *(char ***)vector_at(vector, idx);
+	if (str[column])
+	{
+		x = ft_atof(str[column]);
+		z = ft_atof(str[column]);
+		if (str[column + 1])
+		{
+			y = ft_atof(str[column + 1]);
+			shape_scale(obj, x, y, z);
+		}
+	}
+}
 
 void	create_cylinder(t_vector vector, t_scene *scene, int idx)
 {
 	t_shape	cylinder;
 
 	cylinder = shape_new_cylinder(-1, 1, true);
-	cylinder.material = material_color_apply(vector, idx, 3);
+	cylinder.material = material_color_apply(vector, idx, 5);
 	cylinder.material.color = color_apply(cylinder, vector, 3, 2);
 	position_obj(&vector, &cylinder, idx, 1);
-	shape_scale(&cylinder, 10, 25, 10);
-	shape_rotate(&cylinder, 90, 0, 0);
+	diam_height_obj(&vector, &cylinder, idx, 3);
+	rotation_obj(&vector, &cylinder, idx, 2);
 	tf_compute(&cylinder.transform);
 	vector_pushback(&scene->objs, &cylinder);
 }
